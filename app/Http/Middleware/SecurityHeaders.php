5072fd59e771165bbdd1abc339e42f86b0b8c4cc
elector280 <mauricioflozadao@gmail.com>
2026-04-28 10:04:47 -0400

    SEO: Agregar headers HSTS y X-Frame-Options

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -20,6 +20,8 @@ class SecurityHeaders
         if ($response instanceof \Symfony\Component\HttpFoundation\Response) {
             $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
             $response->headers->set('X-Content-Type-Options', 'nosniff');
+            $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+            $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
             // Adding a permissive CSP to ensure we don't break existing scripts but satisfy the SEO requirement
             $response->headers->set('Content-Security-Policy', "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;");
         }
